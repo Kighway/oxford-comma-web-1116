d6433eb800ca6ae13ab47670678d8d1f2d2fada7
Kyle Tulod <tulod.ky13@gmail.com>
2016-11-22 17:54:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  case array.length
+  when 0
+    return array[0]
+  when 1
+    return array.join(" and ")
+  else
+    oxford_list = array[0]
+    item = 1
+    last = array.length - 1
+    while item != last
+      oxford_list += ", " + array[item]
+      item += 1
+    end
+    oxford_list += ", and " +  array[item]
+  end
 end
